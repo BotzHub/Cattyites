993fd5f145dc1585e5d42e5738bb63f68dea923c
TgCat <99109058+TgCat@users.noreply.github.com>
2025-05-31 10:47:20 +0000

    pylint: auto fixes

--- sample_config.py
+++ sample_config.py
@@ -21,7 +21,7 @@ class Config(object):
     DB_URI = os.environ.get("DATABASE_URL", None)
 
     VPS_NOLOAD = list(os.getenv("VPS_NOLOAD", "").split())
-    
+
     # Get this value by running python3 stringsetup.py or https://repl.it/@sandeep1709/generatestringsession
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     # Telegram BOT Token and bot username from @BotFather
