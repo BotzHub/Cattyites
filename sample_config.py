e1d3be5a14bd08d914a3a3e8e79675e016fcd1f5
Botz Hub <botzhub01@gmail.com>
2025-05-31 16:27:35 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -19,9 +19,8 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH") or None
     # Datbase url heroku sets it automatically else get this from elephantsql
     DB_URI = os.environ.get("DATABASE_URL", None)
-
-    VPS_NOLOAD = list(os.getenv("VPS_NOLOAD", "").split())
-
+    # Empty list means load all plugins
+    VPS_NOLOAD = [] 
     # Get this value by running python3 stringsetup.py or https://repl.it/@sandeep1709/generatestringsession
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     # Telegram BOT Token and bot username from @BotFather
